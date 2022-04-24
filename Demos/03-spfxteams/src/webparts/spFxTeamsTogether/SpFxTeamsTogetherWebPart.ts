eb0193dc376bd25f05a3cd8ea7b620690c6a2f84
Rob Windsor <rob.windsor@gmail.com>
2022-04-24 08:24:28 -0400

    FY22Q4 refresh
    - run through all exercise steps in associated MSLearn module to validate good working order
    - updated steps & fixed typos where necessary
    - implement changes per Acrolinx feedback
    - any & all users/names/company names/pictures/IDs are not real people/tenants... all are from M365 demo tenants provided by Microsoft
    - update module to reflect changes to SPFx v1.14

--- Demos/03-spfxteams/src/webparts/spFxTeamsTogether/SpFxTeamsTogetherWebPart.ts
+++ Demos/03-spfxteams/src/webparts/spFxTeamsTogether/SpFxTeamsTogetherWebPart.ts
@@ -7,12 +7,11 @@ import {
   PropertyPaneTextField
 } from '@microsoft/sp-property-pane';
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import { IReadonlyTheme } from '@microsoft/sp-component-base';
 import { escape } from '@microsoft/sp-lodash-subset';
 
 import styles from './SpFxTeamsTogetherWebPart.module.scss';
 import * as strings from 'SpFxTeamsTogetherWebPartStrings';
-
-import * as microsoftTeams from '@microsoft/teams-js';
 
 export interface ISpFxTeamsTogetherWebPartProps {
   description: string;
@@ -20,41 +19,78 @@ export interface ISpFxTeamsTogetherWebPartProps {
 }
 
 export default class SpFxTeamsTogetherWebPart extends BaseClientSideWebPart<ISpFxTeamsTogetherWebPartProps> {
-  private teamsContext: microsoftTeams.Context;
+
+  private _isDarkTheme: boolean = false;
+  private _environmentMessage: string = '';
 
   protected onInit(): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-      if (this.context.sdks.microsoftTeams) {
-        this.teamsContext = this.context.sdks.microsoftTeams.context;
-      }
-      resolve();
-    });
+    this._environmentMessage = this._getEnvironmentMessage();
+
+    return super.onInit();
   }
 
   public render(): void {
-    let title: string = (this.teamsContext)
-      ? 'Teams'
-      : 'SharePoint';
-    let currentLocation: string = (this.teamsContext)
-      ? `Team: ${this.teamsContext.teamName}`
-      : `site collection ${this.context.pageContext.web.title}`;
-
     this.domElement.innerHTML = `
-      <div class="${styles.spFxTeamsTogether}">
-        <div class="${styles.container}">
-          <div class="${styles.row}">
-            <div class="${ styles.column }">
-              <span class="${ styles.title }">Welcome to ${ title }!</span>
-              <p class="${ styles.subTitle }">Currently in the context of the following ${ currentLocation }</p>
-              <p class="${ styles.description }">${escape(this.properties.description)}</p>
-              <p class="${ styles.description }">${escape(this.properties.customSetting)}</p>
-              <a href="https://aka.ms/spfx" class="${ styles.button }">
-                <span class="${ styles.label }">Learn more</span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>`;
+    <section class="${styles.spFxTeamsTogether} ${!!this.context.sdks.microsoftTeams ? styles.teams : ''}">
+      <div class="${styles.welcome}">
+        <img alt="" src="${this._isDarkTheme ? require('./assets/welcome-dark.png') : require('./assets/welcome-light.png')}" class="${styles.welcomeImage}" />
+        <h2>Well done, ${escape(this.context.pageContext.user.displayName)}!</h2>
+        <div>${this._environmentMessage}</div>
+        <div>Description property value: <strong>${escape(this.properties.description)}</strong></div>
+        <div>Custom setting property value: <strong>${escape(this.properties.customSetting)}</strong></div>        
+      </div>
+      <div>
+        <h3>Welcome to SharePoint Framework!</h3>
+        <p>
+        The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It's the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
+        </p>
+        <h4>Learn more about SPFx development:</h4>
+          <ul class="${styles.links}">
+            <li><a href="https://aka.ms/spfx" target="_blank">SharePoint Framework Overview</a></li>
+            <li><a href="https://aka.ms/spfx-yeoman-graph" target="_blank">Use Microsoft Graph in your solution</a></li>
+            <li><a href="https://aka.ms/spfx-yeoman-teams" target="_blank">Build for Microsoft Teams using SharePoint Framework</a></li>
+            <li><a href="https://aka.ms/spfx-yeoman-viva" target="_blank">Build for Microsoft Viva Connections using SharePoint Framework</a></li>
+            <li><a href="https://aka.ms/spfx-yeoman-store" target="_blank">Publish SharePoint Framework applications to the marketplace</a></li>
+            <li><a href="https://aka.ms/spfx-yeoman-api" target="_blank">SharePoint Framework API reference</a></li>
+            <li><a href="https://aka.ms/m365pnp" target="_blank">Microsoft 365 Developer Community</a></li>
+          </ul>
+      </div>
+    </section>`;
+  }
+
+  private _getEnvironmentMessage(): string {
+    let message: string = "";
+
+    if (!!this.context.sdks.microsoftTeams) { // running in Teams
+      message = this.context.isServedFromLocalhost ?
+        strings.AppLocalEnvironmentTeams :
+        strings.AppTeamsTabEnvironment;
+
+      message += ". Team name: " + this.context.sdks.microsoftTeams.context.teamName;
+    } else {
+      message = this.context.isServedFromLocalhost ?
+        strings.AppLocalEnvironmentSharePoint :
+        strings.AppSharePointEnvironment;
+
+      message += ". Site name: " + this.context.pageContext.web.title;
+    }
+
+    return message;
+  }
+
+  protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
+    if (!currentTheme) {
+      return;
+    }
+
+    this._isDarkTheme = !!currentTheme.isInverted;
+    const {
+      semanticColors
+    } = currentTheme;
+    this.domElement.style.setProperty('--bodyText', semanticColors.bodyText);
+    this.domElement.style.setProperty('--link', semanticColors.link);
+    this.domElement.style.setProperty('--linkHovered', semanticColors.linkHovered);
+
   }
 
   protected get dataVersion(): Version {
